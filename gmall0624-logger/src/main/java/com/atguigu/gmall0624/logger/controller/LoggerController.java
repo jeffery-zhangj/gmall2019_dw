c5211235eeb76220d6a555b70ae7a1d0a650c63c
zj <949733118@qq.com>
2019-11-25 19:11:31 +0800

    Initial commit

---
+++ gmall0624-logger/src/main/java/com/atguigu/gmall0624/logger/controller/LoggerController.java
@@ -0,0 +1,31 @@
+package com.atguigu.gmall0624.logger.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class LoggerController {
+
+    @PostMapping
+    public String log(@RequestParam("logString") String logString) {
+        System.out.println(logString);
+
+        //补时间戳
+        JSONObject jsonObject = JSON.parseObject(logString);
+        jsonObject.put("ts", System.currentTimeMillis());
+
+        //落盘日志文件
+        String jsonString = jsonObject.toJSONString();
+        log.info(jsonString);
+
+        //发送kafka
+
+        return "success";
+    }
+}
---
+++ gmall0624-logger/src/test/java/com/atguigu/gmall0624/logger/Gmall0624LoggerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.atguigu.gmall0624.logger;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gmall0624LoggerApplicationTests {
+
+
+    void contextLoads() {
+    }
+
+}
